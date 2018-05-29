69f913f5f97fa15edf31219b35a852eab6f34d59
Tulio Martins <tuliomartinstm1523@gmail.com>
2018-05-28 21:42:36 -0300

    todas as funcoes funcionais e comentadas

--- projeto2/projeto2.hs
+++ projeto2/projeto2.hs
@@ -1,3 +1,6 @@
+--Tulio Brandao Soares Martins ra:177761
+--Luis Fernando Vieira Silva   ra:
+
 
 import Data.Bool
 import Data.List
@@ -28,6 +31,7 @@ xor = (/=)
 
 --Removing the k biggest edges in the graph
 
+--encontra todas as componentes conexas e as lista em uma lista de componentes
 cC [] _ = []
 cC (x:xs) edges
   | rest == [] = [components]
@@ -36,38 +40,43 @@ cC (x:xs) edges
   rest   = (x:xs) \\ components
   components = dFS (x:xs) edges [x]
 
+--executa o DFS para encontrar uma componente conexa
 dFS _ _ [] = []
 dFS v e (first:rest)
-  | ([x|x<-v, x==first] == []) = dFS remain e rest
-  | otherwise                  = first : dFS remain e (adj ++ rest)
-  where
-  adj = [x|(x, y, z)<-e, x == first] ++ [x|(y, x, z)<-e, y == first]
-  remain = [x|x<-v, x/=first]
+    | ([x|x<-v, x==first] == []) = dFS remain e rest
+    | otherwise                  = first : dFS remain e (adj ++ rest)
+    where
+    adj = [x|(x, y, z)<-e, y == first] ++ [x|(y, x, z)<-e, y == first]
+    remain = [x|x<-v, x/=first]
 
---Removes an item from the list returning the updated list
+--Remove um item de uma lista de arestas
 rmv :: Eq a => (a, a, Double) -> [(a, a, Double)] -> [(a, a, Double)]
 rmv _ [] = []
 rmv a (x:xs)
   | a == x    = rmv a xs
   | otherwise = x : rmv a xs
 
---Removes the biggest edge in the tree
+--Remove a maior aresta de uma lista de arestas
 findMax :: Eq a =>  [(a, a, Double)] -> [(a, a, Double)] -> (a, a, Double) ->[(a, a, Double)]
 findMax [] edges res  = rmv res edges
 findMax ((a, b, c):xs) edges (x, y, mx)
     | c > mx    = findMax xs edges (a, b, c)
     | otherwise = findMax xs edges (x, y, mx)
 
-
+--agrupa em clusters o grafo dado pelas arestas removendo as k maiores
+--arestas e devolvendo uma lista de arestas atualizada
 k_grouping :: Eq a => [(a, a, Double)] -> Int -> [(a, a, Double)]
 k_grouping edg 0 = edg
 k_grouping (x:xs) k = k_grouping (findMax (x:xs) (x:xs)  x) (k-1)
 
+--devole a lista de vertices do GraphW
 v_parser (GraphW vs es) = vs
+--devole a lista de arestas do GraphW
 e_parser (GraphW vs es) = es
 
-main = do print $ CC graph_v graph_e
+main = do print $ cC graph_v (k_grouping graph_e 0)
     where
-    graph = prim (GraphW buildGraphV buildGraphE)
+    graph = prim (GraphW [1,2,3,4,5] g5)
     graph_v = v_parser(graph)
     graph_e = e_parser(graph)
+    g5 = [(1,2,12),(1,3,34),(1,5,78),(2,4,55),(2,5,32),(3,4,61),(3,5,44),(4,5,93),(2,1,12),(3,1,34),(5,1,78),(4,2,55),(5,2,32),(4,3,61),(5,3,44),(5,4,93)]
